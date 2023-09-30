3245b7680281ac2e27d8d64b808eca18a0983a8c
Robert-CPX <cpx@robbdev.xyz>
2023-09-30 15:14:47 +0800

    add assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import '../styles/global.css'
+// eslint-disable-next-line camelcase
+import { Inter, Space_Grotesk } from 'next/font/google'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -7,14 +9,28 @@ export const metadata: Metadata = {
   description: 'A stackoverflow clone for developers',
 }
 
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: '--font-inter',
+})
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: '--font-spaceGrotesk',
+})
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={`${inter.variable} ${spaceGrotesk.variable}`}>
+      <body>
+        {children}
+      </body>
     </html>
   )
 }
